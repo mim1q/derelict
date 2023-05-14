cf8c8b1b747597b2070be7fbee13b62348cbbc5a
mim1q <mim1q.dev@gmail.com>
2023-05-14 23:25:15 +0200

    Added particles when using

--- src/main/kotlin/com/github/mim1q/derelict/item/graffiti/SprayCanItem.kt
+++ src/main/kotlin/com/github/mim1q/derelict/item/graffiti/SprayCanItem.kt
@@ -1,18 +1,43 @@
 package com.github.mim1q.derelict.item.graffiti
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.ItemUsage
+import net.minecraft.particle.ParticleTypes
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.util.UseAction
+import net.minecraft.util.math.MathHelper
 import net.minecraft.world.World
 
 class SprayCanItem(settings: FabricItemSettings) : Item(settings.maxCount(1).maxDamage(16)) {
   override fun getUseAction(stack: ItemStack) = UseAction.SPYGLASS
   override fun getMaxUseTime(stack: ItemStack) = 60
-  override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> =
-    ItemUsage.consumeHeldItem(world, user, hand)
+  override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+    if (hand === Hand.OFF_HAND) return TypedActionResult.fail(user.getStackInHand(hand))
+    return ItemUsage.consumeHeldItem(world, user, hand)
+  }
+
+  override fun usageTick(world: World?, user: LivingEntity?, stack: ItemStack?, remainingUseTicks: Int) {
+    if (world?.isClient == true && user != null) {
+      val vec = user.rotationVector
+      val rotatedVec = vec.rotateY(MathHelper.RADIANS_PER_DEGREE * -90)
+      val pos = user.pos.add(
+        rotatedVec.x * 0.15 + vec.x * 0.2,
+        1.75 + vec.y * 0.2,
+        rotatedVec.z * 0.15 + vec.z * 0.2
+      )
+      val rng = user.random
+      val velocity = vec.multiply(
+        0.1 + rng.nextDouble() * 0.1,
+        0.1 + rng.nextDouble() * 0.1,
+        0.1 + rng.nextDouble() * 0.1
+      )
+      val particle = ParticleTypes.SMOKE
+      world.addParticle(particle, pos.x, pos.y, pos.z, velocity.x, velocity.y, velocity.z)
+    }
+  }
 }
