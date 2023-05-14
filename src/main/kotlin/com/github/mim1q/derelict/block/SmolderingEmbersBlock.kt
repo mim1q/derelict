af7a7a56db07096ce3947029461562766a27837c
mim1q <mim1q.dev@gmail.com>
2023-05-14 12:48:22 +0200

    Embers can now be waterlogged

--- src/main/kotlin/com/github/mim1q/derelict/block/SmolderingEmbersBlock.kt
+++ src/main/kotlin/com/github/mim1q/derelict/block/SmolderingEmbersBlock.kt
@@ -1,27 +1,66 @@
 package com.github.mim1q.derelict.block
 
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
-import net.minecraft.block.BlockState
-import net.minecraft.block.LichenGrower
-import net.minecraft.block.MultifaceGrowthBlock
+import net.minecraft.block.*
+import net.minecraft.fluid.FluidState
+import net.minecraft.fluid.Fluids
+import net.minecraft.item.ItemPlacementContext
 import net.minecraft.particle.DefaultParticleType
 import net.minecraft.particle.ParticleTypes
+import net.minecraft.state.StateManager
+import net.minecraft.state.property.BooleanProperty
+import net.minecraft.state.property.Properties
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.util.math.Vec3d
 import net.minecraft.util.math.random.Random
 import net.minecraft.world.World
+import net.minecraft.world.WorldAccess
 
+@Suppress("OVERRIDE_DEPRECATION", "DEPRECATION")
 class SmolderingEmbersBlock(
   settings: FabricBlockSettings,
   private val primaryParticle: DefaultParticleType = ParticleTypes.FLAME,
   private val secondaryParticle: DefaultParticleType = ParticleTypes.SMOKE,
   private val particleVelocity: Double = 0.01,
   private val particleRarity: Float = 0.2f
-) : MultifaceGrowthBlock(
-  settings.noCollision().ticksRandomly()
-) {
+) : MultifaceGrowthBlock(settings.noCollision().ticksRandomly()), Waterloggable {
   override fun getGrower(): LichenGrower = null!!
 
+  // Waterloggable
+  companion object {
+    val WATERLOGGED: BooleanProperty = Properties.WATERLOGGED
+  }
+
+  init {
+    defaultState = defaultState.with(WATERLOGGED, false)
+  }
+
+  override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
+    super.appendProperties(builder)
+    builder.add(WATERLOGGED)
+  }
+
+  override fun getPlacementState(ctx: ItemPlacementContext) =
+    super.getPlacementState(ctx)?.with(WATERLOGGED, false)
+
+  override fun getFluidState(state: BlockState): FluidState {
+    return if (state[WATERLOGGED]) Fluids.WATER.getStill(false) else super.getFluidState(state)
+  }
+
+  override fun getStateForNeighborUpdate(
+    state: BlockState,
+    direction: Direction,
+    neighborState: BlockState,
+    world: WorldAccess,
+    pos: BlockPos,
+    neighborPos: BlockPos
+  ): BlockState {
+    world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world))
+    return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos)
+  }
+
+  // Particle Spawning
   private fun getBaseParticleOffset(state: BlockState, random: Random): Vec3d {
     val directions = collectDirections(state)
     val direction = directions.toList()[random.nextInt(directions.size)]
