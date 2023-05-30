ecaad878190874d32bd2f306018f33ceb61b90dc
mim1q <mim1q.dev@gmail.com>
2023-05-30 20:09:48 +0200

    Added Arachnophobia Filter resource pack

--- src/main/kotlin/com/github/mim1q/derelict/DerelictClient.kt
+++ src/main/kotlin/com/github/mim1q/derelict/DerelictClient.kt
@@ -3,10 +3,20 @@ package com.github.mim1q.derelict
 import com.github.mim1q.derelict.init.ModParticles
 import com.github.mim1q.derelict.init.client.ModRender
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType
+import net.fabricmc.loader.api.FabricLoader
 
 object DerelictClient : ClientModInitializer {
   override fun onInitializeClient() {
     ModRender.init()
     ModParticles.initClient()
+
+    ResourceManagerHelper.registerBuiltinResourcePack(
+      Derelict.id("arachnophobia_filter"),
+      FabricLoader.getInstance().getModContainer("derelict").get(),
+      "Arachnophobia Filter",
+      ResourcePackActivationType.NORMAL,
+    )
   }
 }
