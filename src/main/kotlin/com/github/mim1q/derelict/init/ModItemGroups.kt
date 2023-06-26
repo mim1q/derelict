47890698a4d942264c1505ab96963ad2705d5b33
mim1q <mim1q.dev@gmail.com>
2023-06-26 11:18:59 +0200

    Rearranged items, added missing translations

--- src/main/kotlin/com/github/mim1q/derelict/init/ModItemGroups.kt
+++ src/main/kotlin/com/github/mim1q/derelict/init/ModItemGroups.kt
@@ -2,7 +2,6 @@ package com.github.mim1q.derelict.init
 
 import com.github.mim1q.derelict.Derelict
 import com.github.mim1q.derelict.init.ModBlocksAndItems.ItemCategory
-import com.github.mim1q.derelict.item.tag.ModItemTags
 import io.wispforest.owo.itemgroup.Icon
 import io.wispforest.owo.itemgroup.OwoItemGroup
 import io.wispforest.owo.itemgroup.gui.ItemGroupTab
@@ -11,18 +10,24 @@ import net.minecraft.text.Text
 import net.minecraft.util.collection.DefaultedList
 
 object ModItemGroups {
-  private val ITEM_GROUP_TEXTURE = Derelict.id("textures/gui/derelictitemtab.png")
+  private val ITEM_GROUP_TEXTURE = Derelict.id("textures/gui/item_group_background.png")
+  private val TAB_TEXTURE = Derelict.id("textures/gui/tabs.png")
 
   internal val ITEM_GROUP: OwoItemGroup = OwoItemGroup.builder(Derelict.id("derelict")) {
     ModBlocksAndItems.FLICKERING_REDSTONE_LAMP.asItem().defaultStack
   }.customTexture(ITEM_GROUP_TEXTURE).build().apply {
-    addTab(Icon.of(ModBlocksAndItems.FLICKERING_LANTERN), "general", ModItemTags.GENERAL_TAB, false)
-    tabs.add(1, ItemGroupTab(
-      Icon.of(ModBlocksAndItems.CONSTRUCTION_STEEL.unaffected.block.asItem().defaultStack),
-      Text.translatable("itemGroup.derelict.derelict.tab.metals"),
-      supplyCategories(ItemCategory.METAL_BLOCKS, ItemCategory.METAL_DECORATION, ItemCategory.WAXED_METAL_BLOCKS, ItemCategory.WAXED_METAL_DECORATION),
-      ItemGroupTab.DEFAULT_TEXTURE,
-      false
+    tabs.add(0, ItemGroupTab(
+      Icon.of(ModBlocksAndItems.FLICKERING_LANTERN),
+      Text.translatable("itemGroup.derelict.derelict.tab.general"),
+      supplyCategories(
+        ItemCategory.GENERAL,
+        ItemCategory.METAL_BLOCKS,
+        ItemCategory.METAL_DECORATION,
+        ItemCategory.WAXED_METAL_BLOCKS,
+        ItemCategory.WAXED_METAL_DECORATION
+      ),
+      TAB_TEXTURE,
+      true
     ))
     initialize()
   }
