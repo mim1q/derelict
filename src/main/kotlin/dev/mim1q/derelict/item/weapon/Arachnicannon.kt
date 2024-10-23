e6ed580d1b1a055d547f38a889c95672d4747a36
Mim1q <mim1q.dev@gmail.com>
2024-10-23 16:44:46 +0200

    Added missing lang, made arachnicannon better

--- src/main/kotlin/dev/mim1q/derelict/item/weapon/Arachnicannon.kt
+++ src/main/kotlin/dev/mim1q/derelict/item/weapon/Arachnicannon.kt
@@ -3,29 +3,38 @@ package dev.mim1q.derelict.item.weapon
 import dev.mim1q.derelict.block.cobweb.SpiderEggBlock
 import dev.mim1q.derelict.entity.projectile.SpiderEggProjectile
 import dev.mim1q.derelict.init.ModEntities
+import net.minecraft.client.item.TooltipContext
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.item.ItemUsage
 import net.minecraft.item.ItemUsageContext
+import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvents
-import net.minecraft.util.ActionResult
-import net.minecraft.util.Hand
-import net.minecraft.util.TypedActionResult
+import net.minecraft.text.Text
+import net.minecraft.util.*
 import net.minecraft.world.World
 
 const val MAX_EGGS = 20
+const val MAX_USE_TIME = 60 * 60 * 20
 
 class Arachnicannon(settings: Settings) : Item(settings) {
+    override fun onStoppedUsing(stack: ItemStack, world: World, user: LivingEntity, remainingUseTicks: Int) {
+        super.onStoppedUsing(stack, world, user, remainingUseTicks)
 
-    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
-        if (!world.isClient) {
+        val time = MAX_USE_TIME - remainingUseTicks
 
-            val count = user.getStackInHand(hand).getLoadedEggs()
+        if (!world.isClient && time > 10) {
+            val count = stack.getLoadedEggs()
             val amount = count.coerceAtMost(5)
-            if (amount == 0) return TypedActionResult.fail(user.getStackInHand(hand))
+            if (amount == 0) return
 
-            user.getStackInHand(hand).setLoadedEggs(count - amount)
+            stack.setLoadedEggs(count - amount)
+            stack.damage(1, user) {
+                user.sendToolBreakStatus(user.activeHand)
+            }
 
             repeat(amount) {
                 val projectile = SpiderEggProjectile(ModEntities.SPIDER_EGG_PROJECTILE, world)
@@ -35,11 +44,18 @@ class Arachnicannon(settings: Settings) : Item(settings) {
                 world.spawnEntity(projectile)
             }
 
-            user.itemCooldownManager.set(this, 20)
+            (user as? ServerPlayerEntity)?.itemCooldownManager?.set(this, 200)
         }
-
-        return TypedActionResult.consume(user.getStackInHand(hand))
     }
+
+    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> =
+        user.getStackInHand(hand).let {
+            if (it.getLoadedEggs() == 0) {
+                TypedActionResult.fail(it)
+            } else {
+                ItemUsage.consumeHeldItem(world, user, hand)
+            }
+        }
 
     override fun useOnBlock(context: ItemUsageContext): ActionResult {
         val state = context.world.getBlockState(context.blockPos)
@@ -56,10 +72,32 @@ class Arachnicannon(settings: Settings) : Item(settings) {
             )
             return ActionResult.SUCCESS
         }
-
         return super.useOnBlock(context)
     }
 
+    override fun getMaxUseTime(stack: ItemStack): Int = MAX_USE_TIME
+    override fun getUseAction(stack: ItemStack): UseAction = UseAction.BOW
+
     private fun ItemStack.getLoadedEggs(): Int = nbt?.getInt("eggs") ?: 0
     private fun ItemStack.setLoadedEggs(eggs: Int) = getOrCreateNbt().putInt("eggs", eggs)
+
+    override fun appendTooltip(
+        stack: ItemStack,
+        world: World?,
+        tooltip: MutableList<Text>,
+        context: TooltipContext
+    ) {
+        super.appendTooltip(stack, world, tooltip, context)
+        val loaded = stack.getLoadedEggs()
+        if (loaded == 0) {
+            tooltip.add(
+                Text.translatable("item.derelict.arachnicannon.tooltip.no_eggs")
+                    .formatted(Formatting.GRAY)
+            )
+        }
+        tooltip.add(
+            Text.translatable("item.derelict.arachnicannon.tooltip.eggs", loaded, MAX_EGGS)
+                .formatted(Formatting.GRAY)
+        )
+    }
 }
