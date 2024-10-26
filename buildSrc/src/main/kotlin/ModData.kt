90ab9b9cf429a348e643e0bd314733693bf303c7
Mim1q <mim1q.dev@gmail.com>
2024-10-26 15:21:54 +0200

    Last tweaks before update!

--- buildSrc/src/main/kotlin/ModData.kt
+++ buildSrc/src/main/kotlin/ModData.kt
@@ -2,7 +2,8 @@ object ModData {
     const val group = "dev.mim1q"
     const val id = "derelict"
     const val version = "2.0.0"
-    const val versionType = "release"
+    const val versionType = "beta"
     val mcVersions = listOf("1.20.1")
     val dependencies = listOf("fabric-api", "owo-lib", "fabric-language-kotlin")
+    val embeddedDependencies = listOf("biolith")
 }
